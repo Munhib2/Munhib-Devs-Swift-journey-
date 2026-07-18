5ee8131167c782323bb295663c256c7ac0899826
Munhib Devs <munhib219@gmail.com>
2026-07-18 21:48:01 +0500

    Initial Commit

---
+++ welcomePagePractice/welcomePagePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  welcomePagePracticeApp.swift
+//  welcomePagePractice
+//
+//  Created by Munhib on 18/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct welcomePagePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
