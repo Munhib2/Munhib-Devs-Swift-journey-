f8608172342122c7a2726ad68821e22357756ecd
Munhib <139168082+Munhib2@users.noreply.github.com>
2026-07-20 00:23:06 +0800

    Add files via upload

---
+++ Task-2/CryptoDashboardUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoDashboardUIApp.swift
+//  CryptoDashboardUI
+//
+//  Created by Munhib on 19/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CryptoDashboardUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
